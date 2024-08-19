0aafac6ce5b6efa283ed321e4a69da8f810d1d42
Bereket Engida <bekacru@gmail.com>
2024-08-19 14:57:58 +0300

    fix: types

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -167,8 +167,9 @@ export function createEndpoint<
 
 		if (res && "_flag" in res) {
 			if (res._flag === "json" && internalCtx._flag === "router") {
-				Object.fromEntries(res.response.headers).map(([key, value]) => {
-					responseHeader.set(key, value);
+				const h = res.response.headers as Record<string, string>;
+				Object.keys(h).forEach((key) => {
+					responseHeader.set(key, h[key as keyof typeof h]);
 				});
 				actualResponse = new Response(JSON.stringify(res.response.body), {
 					status: res.response.status ?? 200,
