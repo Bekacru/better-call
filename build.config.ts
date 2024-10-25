63e9b740f1d617fe62325feb936ee5b0bdbfbb04
Bereket Engida <bekacru@gmail.com>
2024-10-25 10:01:23 +0300

    chore: fix bundling issues

--- build.config.ts
+++ build.config.ts
@@ -3,5 +3,5 @@ import { defineBuildConfig } from "unbuild";
 export default defineBuildConfig({
   outDir: "dist",
   declaration: true,
-  externals: ["zod"],
+  externals: ["zod", "@better-fetch/fetch", "undici-types"],
 });
