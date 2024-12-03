3a2de0c9588800ea9e1cf466cd6d3b18311c4e38
Bereket Engida <bekacru@gmail.com>
2024-12-03 19:01:34 +0300

    fix: check for protocol from headers

--- src/adapter/node.ts
+++ src/adapter/node.ts
@@ -5,7 +5,8 @@ import type { Router } from "../router.js";
 
 export function toNodeHandler(handler: Router["handler"]) {
 	return async (req: IncomingMessage, res: ServerResponse) => {
-		const protocol = (req.socket as any).encrypted ? "https" : "http";
+		const protocol =
+			req.headers["x-forwarded-proto"] || ((req.socket as any).encrypted ? "https" : "http");
 		const base = `${protocol}://${req.headers[":authority"] || req.headers.host}`;
 		const response = await handler(getRequest({ base, request: req }));
 		setResponse(res, response);
