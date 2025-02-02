d8b34f800df20ece63c53f07fd11335bc48348e5
Bereket Engida <bekacru@gmail.com>
2025-02-02 10:30:44 +0300

    fix: clone request by default

--- src/router.ts
+++ src/router.ts
@@ -26,6 +26,12 @@ export interface RouterConfig {
 	extraContext?: Record<string, any>;
 	onResponse?: (res: Response) => any | Promise<any>;
 	onRequest?: (req: Request) => any | Promise<any>;
+	/**
+	 * Disable request cloning
+	 *
+	 * @default false
+	 */
+	disableRequestCloning?: boolean;
 }
 
 export const createRouter = <E extends Record<string, Endpoint>, Config extends RouterConfig>(
@@ -69,7 +75,7 @@ export const createRouter = <E extends Record<string, Endpoint>, Config extends 
 		const method = request.method;
 		const route = findRoute(router, method, path);
 		const handler = route?.data as Endpoint;
-		const body = await getBody(request);
+		const body = await getBody(config?.disableRequestCloning ? request : request.clone());
 		const headers = request.headers;
 		const query = Object.fromEntries(url.searchParams);
 		const routerMiddleware = findAllRoutes(middlewareRouter, "*", path);
